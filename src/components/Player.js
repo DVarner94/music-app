82b34d839be43a1d79f329f10ab50d55a219d1f2
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-14 10:19:52 -0800

    player component file created

---
+++ src/components/Player.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Player = () => {
+    return (
+        <div className="player">
+            <div className="time-control">
+                
+            </div>
+        </div>
+    )
+};
+
+export default Player;
