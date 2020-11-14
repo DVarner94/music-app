9e9e3263168595d3f9b8b4f4f4c16986b098c3bd
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-14 10:26:22 -0800

    starting the 'player bar'

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,8 +4,11 @@ const Player = () => {
     return (
         <div className="player">
             <div className="time-control">
-                
+                <p>Start Time</p>
+                <input type="range"/>
+                <p>End Time</p>
             </div>
+            <div className="play-control"></div>
         </div>
     )
 };
