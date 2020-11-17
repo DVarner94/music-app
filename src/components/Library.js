e5559142395c21a7e9d0fdcaea2da30dd39e63d3
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-17 14:18:46 -0800

    can now use audioref in library, passed in other props to use as well

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import LibrarySong from './LibrarySong'
 
-const Library = ({ songs, setCurrentSong }) => {
+const Library = ({ isPlaying, audioRef, songs, setCurrentSong }) => {
     return (
         <div className="library">
             <h2>Library</h2>
             <div className="library-songs">
                 {songs.map((song) =>
-                    <LibrarySong 
-                    songs={songs} 
-                    setCurrentSong={setCurrentSong} 
-                    song={song} 
-                    id={song.id} 
-                    key={song.id} 
+                    <LibrarySong
+                        isPlaying={isPlaying}
+                        songs={songs}
+                        setCurrentSong={setCurrentSong}
+                        song={song}
+                        id={song.id}
+                        key={song.id}
                     />
                 )}
             </div>
