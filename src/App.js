d19ae49ed61b160a2a58423b9197490e46a3d048
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-17 14:18:16 -0800

    lifting state so we can use audioRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 // import styles
 import './styles/app.scss';
 // import components
@@ -11,18 +11,24 @@ import data from './data';
 
 
 function App() {
+  // ref
+  const audioRef = useRef(null);
+
   // State 
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
-  const [isPlaying, setIsPlaying] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player setIsPlaying={setIsPlaying}
+      <Player
+        setIsPlaying={setIsPlaying}
         isPlaying={isPlaying}
         currentSong={currentSong}
+        audioRef={audioRef}
       />
-      <Library songs={songs} setCurrentSong={setCurrentSong} />
+      <Library songs={songs} setCurrentSong={setCurrentSong} setSongs={setSongs} isPlaying={isPlaying} audioRef={audioRef} />
     </div>
   );
 }
