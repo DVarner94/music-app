c7271f02cb0a93641464fcc61d1c3702ed4f20ee
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-16 21:46:21 -0800

    bringing in Library

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles/app.scss';
 // import components
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library';
 // import songs
 import data from './data';
 
@@ -17,7 +18,11 @@ function App() {
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong} />
+      <Player setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
+      <Library songs={songs} />
     </div>
   );
 }
