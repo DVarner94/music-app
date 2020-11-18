f07c4ca1dc6eac186a5da4adf54669b0b9a3c187
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-17 16:00:15 -0800

    brought methods from Player to App

--- src/App.js
+++ src/App.js
@@ -18,17 +18,44 @@ function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration })
+  };
 
   return (
     <div className="App">
-      <Song currentSong={currentSong} />
+      <Song isPlaying={isPlaying} currentSong={currentSong} />
       <Player
         setIsPlaying={setIsPlaying}
         isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
         currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
         audioRef={audioRef}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+        songs={songs}
+        setSongs={setSongs}
       />
-      <Library songs={songs} setCurrentSong={setCurrentSong} setSongs={setSongs} isPlaying={isPlaying} audioRef={audioRef} />
+      <Library
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        setSongs={setSongs}
+        isPlaying={isPlaying}
+        audioRef={audioRef} />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
+
     </div>
   );
 }
