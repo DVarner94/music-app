67b4236c54aeb8f8fa459cfc5d77b81570b0ec9b
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-17 20:27:23 -0800

    creating library status so we can open/close

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,9 +1,9 @@
 import React, { useRef } from 'react';
 import LibrarySong from './LibrarySong'
 
-const Library = ({ isPlaying, audioRef, songs, setCurrentSong, setSongs }) => {
+const Library = ({ isPlaying, audioRef, songs, setCurrentSong, setSongs, libraryStatus }) => {
     return (
-        <div className="library">
+        <div className={`library ${libraryStatus ? 'active-library' : ''}`}>
             <h2>Library</h2>
             <div className="library-songs">
                 {songs.map((song) =>
